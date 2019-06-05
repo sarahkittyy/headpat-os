5a19e2f5cd4781a4fa8e651a88cdaf95e3246f64
sarahkittyy <chrisohlin1@gmail.com>
2019-06-05 19:35:18 -0400

    added math header

---
+++ include/std/math.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "std/common.h"
+
+/**
+ * @brief Get the absolute value of a number.
+ * 
+ * @param in The number
+ * @return int The absolute value of that number.
+ */
+int abs(int in);
---
+++ src/std/math.c
@@ -0,0 +1,6 @@
+#include "std/math.h"
+
+int abs(int in)
+{
+	return in > 0 ? in : -in;
+}
