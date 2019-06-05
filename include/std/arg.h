7b9810e4b80e91e83aad4a88df982cb25ffe1dd9
sarahkittyy <chrisohlin1@gmail.com>
2019-06-05 19:49:00 -0400

    refactored slightly

--- include/std/arg.h
+++ include/std/arg.h
@@ -1,7 +1,7 @@
 #pragma once
 
 typedef char* va_list;
-#define va_start(args, fmt) args = (char*)(&args) + sizeof(&args)
+#define va_start(args, start) args = (char*)(&args) + sizeof(&args)
 #define va_arg(args, type) \
 	*((type*)args);        \
 	args += sizeof(type)
