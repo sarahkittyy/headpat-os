43a8601aa3c4d93406519cb44ebf76ddd6a1d9a7
sarahkittyy <chrisohlin1@gmail.com>
2019-06-05 19:35:26 -0400

    added more string functions

--- include/std/string.h
+++ include/std/string.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "std/common.h"
+#include "std/math.h"
 
 /**
  * @brief Get the length of a null-terminated string.
@@ -9,3 +10,47 @@
  * @return uint32_t The length.
  */
 size_t strlen(const char* str);
+
+/**
+ * @brief Concatenate n bytes of src onto dest.
+ * 
+ * @param dest The destination string.
+ * @param src The source string
+ * @param n The amount of bytes
+ */
+void strncat(char* dest, const char* src, size_t n);
+
+/**
+ * @brief Concatenate a string onto another.
+ * 
+ * @param dest The destination string.
+ * @param src The source string.
+ */
+void strcat(char* dest, const char* src);
+
+/**
+ * @brief Convert an integer to a string.
+ * 
+ * @param dest Where the string will be written to.
+ * @param input The input integer.
+ * @param base The base of the integer.
+ * @return char* `dest`
+ */
+char* itoa(char* dest, int input, int base);
+
+/**
+ * @brief Reverse a string in-place.
+ * 
+ * @param in The string to reverse.
+ */
+void reverse(char* in);
+
+/**
+ * @brief Generates a formatted string according to `fmt` and the
+ * following arguments, and puts it into `dest`.
+ * 
+ * @param dest The resulting string.
+ * @param fmt The formatted string.
+ * @param ... The arguments according to `fmt`
+ */
+void sprintf(char* dest, const char* fmt, ...);
--- src/std/string.c
+++ src/std/string.c
@@ -10,3 +10,137 @@ size_t strlen(const char* str)
 	}
 	return size;
 }
+
+/**
+ * @brief Converts a number to a digit.
+ * 
+ * @param number The number to convert
+ * @return char That number's corresponding digit.
+ */
+char todigit(uint8_t number)
+{
+	static const char digits[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	if (number > sizeof(digits))
+	{
+		return '-';
+	}
+	else
+	{
+		return digits[number];
+	}
+}
+
+char* itoa(char* dest, int input, int base)
+{
+	//Special case for 0.
+	if (input == 0)
+	{
+		dest[0] = '0';
+		return dest;
+	}
+
+	int sign = input / abs(input);
+	input	= abs(input);
+	for (int i = 0; input != 0; ++i)
+	{
+		int d   = input % base;
+		dest[i] = todigit(d);
+		input /= base;
+	}
+	//Append a - if the sign is -
+	if (sign == -1)
+	{
+		dest[strlen(dest)] = '-';
+	}
+
+	//Reverse the string.
+	reverse(dest);
+	return dest;
+}
+
+void reverse(char* in)
+{
+	size_t len = strlen(in);
+
+	for (size_t i = 0; i < len / 2; ++i)
+	{
+		char t			= in[i];
+		in[i]			= in[len - i - 1];
+		in[len - i - 1] = t;
+	}
+}
+
+void strncat(char* dest, const char* src, size_t n)
+{
+	size_t dest_start = strlen(dest);
+	for (size_t i = 0; i < n; ++i)
+	{
+		dest[dest_start + i] = src[i];
+	}
+}
+
+void strcat(char* dest, const char* src)
+{
+	//Just call strncat.
+	strncat(dest, src, strlen(src));
+}
+
+void sprintf(char* dest, const char* fmt, ...)
+{
+	//Get a pointer to the first argument.
+	char* va_arg = (char*)(&fmt) + sizeof(fmt);
+
+	//Iterate over the fmt string.
+	size_t len = strlen(fmt);
+	int dest_i = 0;   //Dest vs Format index
+	for (size_t fmt_i = 0; fmt_i < len; ++fmt_i)
+	{
+		char current = fmt[fmt_i];
+		//If the current character is a % sign.
+		if (current == '%')
+		{
+			//Get the next character.
+			char type = fmt[fmt_i + 1];
+			switch (type)
+			{
+			default:
+				break;
+			case '%':
+				dest[dest_i] = '%';
+				dest_i++;
+				break;
+			case 'i':
+			{
+				//Get the next va_arg as an integer.
+				int arg = *((int*)va_arg);
+				va_arg += sizeof(int);
+				//Concatenate it onto the result.
+				char str[11] = {0};
+				itoa(str, arg, 10);
+				strcat(dest, str);
+				dest_i += strlen(str);
+				break;
+			}
+			case 's':
+			{
+				const char* arg = *((const char**)va_arg);
+				va_arg += sizeof(const char*);
+				strcat(dest, arg);
+				dest_i += strlen(arg);
+				break;
+			}
+			}
+
+			//Skip the next character.
+			fmt_i++;
+		}
+		else
+		{
+
+			//Copy the character to the destination.
+			dest[dest_i] = current;
+			dest_i++;
+		}
+	}
+}
