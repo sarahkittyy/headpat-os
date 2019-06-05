71e7f9d1681ace55d5d061548f9278898894e0d7
sarahkittyy <chrisohlin1@gmail.com>
2019-06-05 18:54:35 -0400

    added some comments

--- include/kernel/isr.h
+++ include/kernel/isr.h
@@ -3,40 +3,6 @@
 #include "kernel/idt.h"
 #include "std/serial.h"
 
-#define _externISR(x) void _isr_##x()
-_externISR(0);
-_externISR(1);
-_externISR(2);
-_externISR(3);
-_externISR(4);
-_externISR(5);
-_externISR(6);
-_externISR(7);
-_externISR(8);
-_externISR(9);
-_externISR(10);
-_externISR(11);
-_externISR(12);
-_externISR(13);
-_externISR(14);
-_externISR(15);
-_externISR(16);
-_externISR(17);
-_externISR(18);
-_externISR(19);
-_externISR(20);
-_externISR(21);
-_externISR(22);
-_externISR(23);
-_externISR(24);
-_externISR(25);
-_externISR(26);
-_externISR(27);
-_externISR(28);
-_externISR(29);
-_externISR(30);
-_externISR(31);
-
 /**
  * @brief Data representing the state of the system on an interrupt catch.
  * 
--- src/kernel/isr.c
+++ src/kernel/isr.c
@@ -1,9 +1,49 @@
 #include "kernel/isr.h"
 
+//Load the external interrupt service routine procedures
+#define externISR(x) extern void _isr_##x()
+externISR(0);
+externISR(1);
+externISR(2);
+externISR(3);
+externISR(4);
+externISR(5);
+externISR(6);
+externISR(7);
+externISR(8);
+externISR(9);
+externISR(10);
+externISR(11);
+externISR(12);
+externISR(13);
+externISR(14);
+externISR(15);
+externISR(16);
+externISR(17);
+externISR(18);
+externISR(19);
+externISR(20);
+externISR(21);
+externISR(22);
+externISR(23);
+externISR(24);
+externISR(25);
+externISR(26);
+externISR(27);
+externISR(28);
+externISR(29);
+externISR(30);
+externISR(31);
+
+//
 #define setIDTEntryNum(x) setIDTEntry(x, (uint32_t)_isr_##x, 0b10001110)
 
 void initISRS()
 {
+	/*
+	 * Initializs all Interrupt descriptor table entries with pointers to the asm interrupt labels,
+	 * which push cpu data and call the generic fault handler.
+	 */
 	setIDTEntryNum(0);
 	setIDTEntryNum(1);
 	setIDTEntryNum(2);
@@ -38,7 +78,14 @@ void initISRS()
 	setIDTEntryNum(31);
 }
 
+/**
+ * @brief The generic fault handler, called by all generic isrs.
+ * 
+ * @param fd 
+ */
 void handleFault(struct FaultData* fd)
 {
 	comWrite(COM1, "FAULT HANDLED");
+	for (;;)
+		;
 }
