a66affe0d8676fecd5f189067413622a8dc5ba90
sarahkittyy <chrisohlin1@gmail.com>
2019-06-06 08:50:15 -0400

    simplified va_arg

--- include/std/arg.h
+++ include/std/arg.h
@@ -3,7 +3,8 @@
 typedef char* va_list;
 #define va_start(args, start) args = (char*)(&start) + sizeof(start)
 #define va_arg(args, type) \
-	(args += sizeof(type), *((type*)(void*)(args - sizeof(type))))
+	*((type*)(args));      \
+	args += sizeof(type)
 
 #define va_copy(dest, src) ((void)(dest = src))
 #define va_end(args) args = NULL
